9d7a0e5fd50208c03e84d89a855a642280744885
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-19 10:31:26 -0300

    feat(channels.scm): add the nonguix channel

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,4 +1,15 @@
 (list (channel
+       (name 'nonguix)
+       (url "https://gitlab.com/nonguix/nonguix")
+       (branch "master")
+       (commit
+        "418e26d189aef24bd4e564a223a751abb25e3de0")
+       (introduction
+        (make-channel-introduction
+         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+         (openpgp-fingerprint
+          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
        (branch "master")
